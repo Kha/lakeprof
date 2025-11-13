dff451d8d145f130535c2a1487bafe558e060557
Garmelon <joscha@plugh.de>
2025-11-13 17:58:05 +0100

    Set up uv and add --json flag to lakeprof report (#1)
    * Set up uv
    
    * Add --json option to lakeprof report

--- flake.nix
+++ flake.nix
@@ -1,19 +1,38 @@
 {
   description = "A Lake build graph profiler";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    with import nixpkgs { inherit system; };
-    rec {
-      packages.lakeprof = python3Packages.buildPythonApplication {
-        propagatedBuildInputs = with python3Packages; [ networkx pydot click moreutils tabulate ];
-        name = "lakeprof";
-        src = ./.;
-      };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages.lakeprof = pkgs.python3Packages.buildPythonApplication {
+          name = "lakeprof";
+          src = ./.;
 
-      defaultPackage = packages.lakeprof;
-    }
-  );
+          pyproject = true;
+          build-system = with pkgs.python3Packages; [ setuptools ];
+          dependencies = with pkgs.python3Packages; [
+            click
+            networkx
+            pydot
+            tabulate
+          ];
+
+          propagatedBuildInputs = with pkgs; [ moreutils ];
+        };
+
+        defaultPackage = packages.lakeprof;
+      }
+    );
 }
